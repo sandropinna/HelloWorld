e42ffb9b89d7eee6816ba331077f648be24aba4a
Sandro <sandro.pinna@gmail.com>
2013-05-22 10:18:45 +0200

    Added print 1 2 3 4 !

--- HelloWorld/src/main/Main.java
+++ HelloWorld/src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 public class Main {
 	public static void main(String[] args){
 		System.out.println("1 2 !");
-		System.out.println("3 4!");			
+		System.out.println("3 4!");		
+		System.out.println("1 2 3 4 !");
 	}
 }
