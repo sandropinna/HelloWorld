3c1e01096b65ae95ec8dfb19247024cec9e3665c
Sandro <sandro.pinna@gmail.com>
2013-05-23 14:57:18 +0200

    branch2 0

--- HelloWorld/src/main/Main.java
+++ HelloWorld/src/main/Main.java
@@ -2,8 +2,6 @@ package main;
 
 public class Main {
 	public static void main(String[] args){
-		System.out.println("master 0");
-		System.out.println("master 1");
-		System.out.println("master 2");
+		System.out.println("branch2 0");		
 	}
 }
