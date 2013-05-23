57baccc1b35ba10cd09200db068d20a8359b0863
Sandro <sandro.pinna@gmail.com>
2013-05-23 14:44:00 +0200

    added print "master 0"

--- HelloWorld/src/main/Main.java
+++ HelloWorld/src/main/Main.java
@@ -2,10 +2,6 @@ package main;
 
 public class Main {
 	public static void main(String[] args){
-		System.out.println("1 2 !");
-		System.out.println("3 4!");		
-		System.out.println("1 2 3 4 !");
-		System.out.println("W il master!");
-		System.out.println("W il master2!");
+		System.out.println("master 0");
 	}
 }
