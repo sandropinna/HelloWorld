bcc7c2f1eb33ec80ac288c4d2ef8d38fe3873298
sandropinna <sandro.pinna@gmail.com>
2013-05-25 19:06:20 +0300

    Update Main.java
    master 0 2 3 4 5

--- HelloWorld/src/main/Main.java
+++ HelloWorld/src/main/Main.java
@@ -2,9 +2,7 @@ package main;
 
 public class Main {
 	public static void main(String[] args){
-		System.out.println("branch1 0");
-		System.out.println("branch1 1");
-		System.out.println("branch1 2");
+	        System.out.println("master 0");
 		System.out.println("master 2");
 		System.out.println("master 3");
 		System.out.println("master 4");
