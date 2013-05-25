1a26affb0ae0a466ce5aa845e9779b6146774ea8
Sandro <sandro.pinna@gmail.com>
2013-05-25 18:28:38 +0200

    master 0 1 2 3 4 5

--- HelloWorld/src/main/Main.java
+++ HelloWorld/src/main/Main.java
@@ -2,10 +2,11 @@ package main;
 
 public class Main {
 	public static void main(String[] args){
-	        System.out.println("master 0");
+	    System.out.println("master 0");
+	    System.out.println("master 1");
 		System.out.println("master 2");
 		System.out.println("master 3");
 		System.out.println("master 4");
-		System.out.println("master 5");
+		System.out.println("master 5");		
 	}
 }
